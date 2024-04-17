178af9010694ad16fbca31dfa46e2c20a1c3ac8e
axelblack4ever <145280362+axelblack4ever@users.noreply.github.com>
2024-04-17 17:55:22 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AxelSystem',
+  webDir: 'www'
+};
+
+export default config;
